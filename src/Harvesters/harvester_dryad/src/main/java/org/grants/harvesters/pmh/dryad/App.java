33f0a364b3c6a6dfcb1625de4ae2e547af4072ec
Dmitrij Kudriavcev <dmitrij@kudriavcev.info>
2014-10-30 20:29:49 +1100

    Harvester classes has been relocated into src/ Added documentations to Harvester classes

---
+++ src/Harvesters/harvester_dryad/src/main/java/org/grants/harvesters/pmh/dryad/App.java
@@ -0,0 +1,49 @@
+package org.grants.harvesters.pmh.dryad;
+
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.grants.harvesters.pmh.Harvester;
+import org.grants.harvesters.pmh.MetadataFormat;
+import org.grants.harvesters.pmh.MetadataPrefix;
+
+/**
+ * Dryad Harvesting Utility
+ * @author Dmitrij Kudriavcev, dmitrij@kudriavcev.info
+ * @version 1.0.1
+ */
+public class App {
+	private static final String REPO_URI = "http://www.datadryad.org/oai/request";
+	private static final String FOLDER_XML = "dryad/xml";
+	
+	public static void main(String[] args) {
+		
+		// init server uri
+		String repoUri = REPO_URI;
+		if (args.length > 0 && !args[0].isEmpty())
+			repoUri = args[0];
+		
+		// init folder address
+		String folderXml = FOLDER_XML;
+		if (args.length > 1 && !args[1].isEmpty())
+			folderXml = args[1];
+		
+		try {
+			// Create harvester object
+			Harvester harvester = new Harvester(repoUri, folderXml);
+
+			// List supported formats
+			List<MetadataFormat> formats = harvester.listMetadataFormats();
+			System.out.println("Supported metadata formats:");
+			for (MetadataFormat format : formats) {
+				System.out.println(format.toString());
+			}
+			
+			// harvest
+			harvester.harvest(MetadataPrefix.mets);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
