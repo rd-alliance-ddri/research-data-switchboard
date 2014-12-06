fa6432a1673b813cfa9433aa23197829efbc8df4
Dmitrij Kudriavcev <dmitrij@kudriavcev.info>
2014-12-06 14:29:47 +1100

    Added Pattern Importer and Pattern CSV file

---
+++ v2/src/Importers/import_patterns/src/main/java/org/grants/importers/patterns/App.java
@@ -0,0 +1,24 @@
+package org.grants.importers.patterns;
+
+public class App {
+	private static final String PATTERNS_SCV = "data/patterns.csv";
+	private static final String NEO4J_URL = "http://localhost:7476/db/data/";
+
+	/**
+	 * Main class function
+	 * @param args String[] Expected to have path to the institutions.csv file and Neo4J URL.
+	 * If missing, the default parameters will be used.
+	 */
+	public static void main(String[] args) {
+		String patternsCsv = PATTERNS_SCV;
+		if (args.length > 0 && null != args[0] && !args[0].isEmpty())
+			patternsCsv = args[0];
+		
+		String neo4jUrl = NEO4J_URL;
+		if (args.length > 1 && null != args[1] && !args[1].isEmpty())
+			neo4jUrl = args[1];
+		
+		Importer importer = new Importer(neo4jUrl);
+		importer.importPatterns(patternsCsv);
+	}
+}
